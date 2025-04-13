fb4c031d51f7b7c4915e519c466f5d392fc4c7fd
Alexandre Fenyo <github@fenyo.net>
2025-04-13 15:25:04 +0200

    Initial Commit

---
+++ SnmpGui/SnmpGuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnmpGuiApp.swift
+//  SnmpGui
+//
+//  Created by Alexandre Fenyo on 13/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct SnmpGuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
